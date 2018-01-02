83de0ce1f66486fedf93e82308f64c3e0102b9b1
szy0syz <szy0syz@gmail.com>
2018-01-02 23:17:05 +0800

    update 打包CSS文件

--- src/entry.js
+++ src/entry.js
@@ -1,1 +1,2 @@
+import css from './css/index.css'
 document.getElementById("title").innerHTML = "Hello Webpack! 2018-01-02";
--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,14 @@ module.exports = {
     filename: 'bundle.js'
   },
   // 模块对象：打包css、转换图片等
-  module: {},
+  module: {
+    rules:[
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
   // 插件必须是数组
   plugins: [],
   devServer: {
