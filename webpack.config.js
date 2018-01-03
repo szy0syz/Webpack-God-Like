6885e561697e88936f70afa491e6fa9f1b50fef9
Jerry Shi <szy0syz@gmail.com>
2018-01-03 14:31:09 +0800

    update  CSS中引用图片

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
-const uglify = require('uglifyjs-webpack-plugin')
+// const uglify = require('uglifyjs-webpack-plugin')
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 const htmlPlugin = require('html-webpack-plugin')
 
 module.exports = {
@@ -12,24 +13,33 @@ module.exports = {
   },
   // 模块对象：打包css、转换图片等
   module: {
-    rules:[
+    rules: [
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif)/,
+        use: [{
+          loader: 'url-loader',
+          options: {
+            limit: 5000
+          }
+        }]
       }
     ]
   },
   // 插件必须是数组
   plugins: [
-    new uglify(),
+    // new uglify(),
+    new UglifyJSPlugin,
     new htmlPlugin({
-      minify:{
-          removeAttributeQuotes:true
+      minify: {
+        removeAttributeQuotes: true
       },
-      hash:true,
-      template:'./src/index.html'
-  })
-  ],
+      hash: true,
+      template: './src/index.html'
+    })],
   devServer: {
     //监听内容的绝对路径
     contentBase: path.resolve(__dirname, 'dist'),
